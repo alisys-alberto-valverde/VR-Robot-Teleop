f05c5a5a8408b5cfbe154a542f896746a2bec81d
alisys-alberto-valverde <44773451+alisys-alberto-valverde@users.noreply.github.com>
2018-11-05 14:15:37 +0100

    Update VR/Assets/Plugins/Connector.cs

--- VR/Assets/Plugins/Connector.cs
+++ VR/Assets/Plugins/Connector.cs
@@ -147,11 +147,11 @@ namespace SharpConnect {
 
 
                 // Encode texture into PNG
-                byte[] bytes = tex.EncodeToPNG();
+                //byte[] bytes = tex.EncodeToPNG();
                
 
                 // For testing purposes, also write to a file in the project folder
-                 File.WriteAllBytes(Application.dataPath + "/../SavedScreen.png", bytes);
+                 //File.WriteAllBytes(Application.dataPath + "/../SavedScreen.png", bytes);
 
 
 
